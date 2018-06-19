caf7c1324740e262c1c4c753913bc5e19fa64d3e
kangwang <2818815189@qq.com>
2018-06-19 23:40:41 +0800

    测试上传

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core';  //引入核心组件
 
-@Component({
+
+@Component({  //声明使用组件的名称
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent {//数据
   title = 'app';
+  h="你好，我是康旺！";
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
+/*
+    如何组装模块的，我们使用的时候首先会先会去加载main.js，然后在加载app.module.ts模块
+
+
+*/
+
+
+//引入模块
+//浏览器解析模块
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+//核心模块
+impo;
+import { HeaderComponent } from './components/header/header.component'rt { NgModule } from '@angular/core';
+//自定义模块
 import { AppComponent } from './app.component';
-
+/,
+    HeaderComponent/接受一个元数据对象，告诉Angular如何编译和启动应用。
+//自定义组件都需要在这里引入和在这里进行注解 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
+  imports: [  //当前项目依赖那些模块
     BrowserModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [],//定义服务
+  bootstrap: [AppComponent] //启动那一个组件
 })
 export class AppModule { }
